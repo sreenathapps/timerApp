829e38a46c6d194a8ed6c3b0141ebd4749ea3f0b
sai <saisreenath.mandaloju@gmail.com>
2025-04-22 17:06:26 +0000

    added basic layout

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -1,1 +1,18 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class DigitalTimer extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <h1 className="main-heading">Digital Timer</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DigitalTimer
